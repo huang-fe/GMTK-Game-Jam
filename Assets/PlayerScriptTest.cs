119e52246c728544381f681182d43dd7b546eb22
Felicity H <felicityh15@gmail.com>
2023-07-09 02:58:59 -0700

    basic working

--- Assets/PlayerScriptTest.cs
+++ Assets/PlayerScriptTest.cs
@@ -1,62 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
 using static UnityEngine.ParticleSystem;
 
 public class PlayerScriptTest : MonoBehaviour
 {
     float timeCount;
     float secondsToApplyForce = 5.0f;
-    bool addingForce = true;
+    float force = 10f;
+    public Vector3 originalPos = new Vector3(0.5f, 0.5f, 0f);
+    bool playing = true;
+    int index = 0;
     Rigidbody m_Rigidbody;
-    //ButtonsPressed[] mvmts;
-    ButtonsPressed m;
+    ButtonsPressed[] mvmts;
 
     void Start()
     {
         //Fetch the Rigidbody from the GameObject with this script attached
         m_Rigidbody = GetComponent<Rigidbody>();
-        m = new ButtonsPressed(false, true, true, secondsToApplyForce);
+        mvmts = new ButtonsPressed[] { new ButtonsPressed(false, false, true, 1.0f), 
+            new ButtonsPressed(false, true, true, 0.5f),
+            new ButtonsPressed(true, true, false, 0.5f),
+            new ButtonsPressed(true, false, false, 0.5f),
+            new ButtonsPressed(true, false, false, 0.5f),
+            new ButtonsPressed(true, false, false, 0.5f),
+            new ButtonsPressed(true, false, false, 0.5f)};
     }
- 
-    void FixedUpdate () {
-        //if(addingForce) {
-        //    timeCount += Time.deltaTime;
 
-        //    if(timeCount < secondsToApplyForce) {
-        //        m_Rigidbody.AddForce(20f, 0, 0);
-        //        Debug.Log(timeCount);
-        //    } else {
-        //        timeCount = 0;
-        //        addingForce = false;   
-        //    }
-        //}
-        if (addingForce)
+    void FixedUpdate()
+    {
+        if (playing)
         {
             timeCount += Time.deltaTime;
 
-            if (timeCount < m.sec)
+            if (timeCount < mvmts[index].sec)
             {
-                if (m.left)
+                if (mvmts[index].left)
                 {
-                    m_Rigidbody.AddForce(-20f, 0, 0);
+                    m_Rigidbody.AddForce(-force, 0, 0);
                 }
-                if (m.right)
+                if (mvmts[index].right)
                 {
-                    m_Rigidbody.AddForce(20f, 0, 0);
+                    m_Rigidbody.AddForce(force, 0, 0);
                 }
-                if (m.up)
+                if (mvmts[index].up)
                 {
-                    m_Rigidbody.AddForce(0, 20f, 0);
+                    m_Rigidbody.AddForce(0, force, 0);
                 }
                 //Debug.Log(timeCount);
             }
             else
             {
                 timeCount = 0;
-                addingForce = false; // index++
+                index++;
+                if (index >= mvmts.Length)
+                {
+                    index = 0;
+                }
             }
         }
+    }
+
+    private void Update()
+    {
+        // nvm cant pause cuz we'd have to turn rigidbody kinematic anyways rip
+        if (Input.GetKeyDown(KeyCode.Space)) // editing mode
+        {
+            resetLvl();
+        }
+    }
+
+    void resetLvl()
+    {
+        transform.position = originalPos;
+        // reset index too
     }
 }
 
